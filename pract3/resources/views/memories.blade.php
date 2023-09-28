3daaf41c48689491e0fc04be2df36ccd844fbee2
Berenice Barcenas Mancilla <berenice.barcenas.mancilla@gmail.com>
2023-09-27 18:50:57 -0600

    pract3 terminada
    se implementaron plantillas con blade manejando las herencias con blade y se hizo la instalacion y configuracion de bootstrap localmente en el proyecto. Tambien se crearon 2 carpetas en views para manejo de las plantillas

---
+++ pract3/resources/views/memories.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.plantilla')
+
+@section('titulo','Memories')
+@section('content')
+    <h1 class="display-1 text-center text-danger mt-4">Memories</h1>    
+@endsection
+
+<!-- -->
--- pract3/routes/web.php
+++ pract3/routes/web.php
@@ -13,6 +13,29 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+
+/*
+|--------------------------------------------------------------------------
+| RUTA DE VISTA PRINCIPAL
+|--------------------------------------------------------------------------
+|
+*/
+Route::get('/', function () { return view('welcome');})->name('inicio');
+
+
+/*
+|--------------------------------------------------------------------------
+| RUTA DE FORMULARIOS
+|--------------------------------------------------------------------------
+|
+*/
+Route::get('/form', function () { return view('form');})->name('formularios'); 
+
+/*
+|--------------------------------------------------------------------------
+| RUTAS DE MEMORIES
+|--------------------------------------------------------------------------
+|
+*/
+Route::get('/memories', function () { return view('memories');})->name('recuerdos'); 
